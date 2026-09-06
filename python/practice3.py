78831146081558b09bfacb4e0393552d64dbef0e
damiemmy <damisaemmanuel778@gmail.com>
2026-09-06 23:57:58 +0100

    python practice

--- python/practice3.py
+++ python/practice3.py
@@ -5,3 +5,8 @@ def calculate_average(scores):
 
 
 print(calculate_average([2,2,2,2,2]))
+
+
+#5. Functions: A professional Python developer should naturally think in functions.
+def calculate_average(scores):
+    return sum(scores) / len(scores)
